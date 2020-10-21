58d0dad5efd1bc38ac9a5bcba847ba6961b0861c
lite1998 <31653932+lite1998@users.noreply.github.com>
2020-10-21 19:05:34 +0530

    Update new easier method.R

--- new easier method.R
+++ new easier method.R
@@ -1,12 +1,17 @@
 read_test_x <- read.table("X_test.txt")
 read_train_X <- read.table("X_train.txt")
+read_subject_test <- read.table("subject_test.txt")
+read_subject_train <- read.table("subject_train.txt")
 read_train_X$Subject <- read_subject_train["V1"]
 read_test_x$Subject <- read_subject_test["V1"]
+read_test_Y <-  read.table("y_test.txt")
+read_train_Y <- read.table("y_train.txt")
 read_test_x$ActivityLabels <- read_test_Y[["V1"]]
 read_train_X$ActivityLabels <- read_train_Y[["V1"]]
 table_test_x <- data.table(read_test_x)
 table_train_x <- data.table(read_train_X)
 read_table_X <- merge.data.table(table_test_x,table_train_x,all.x = TRUE,all.y = TRUE)
+features <- read.table("features.txt")
 names(read_table_X)[1:561] <- features[["V2"]]
 read_table_X$Activity[read_table_X$ActivityLabels=="1"]<- "WALKING"
 read_table_X$Activity[read_table_X$ActivityLabels=="2"]<- "WALKING_UPSTAIRS"
